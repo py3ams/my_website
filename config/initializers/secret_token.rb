de79dc7332f2b6958f574b4378d11c82bc71b671
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-07-17 17:31:59 +0100

    first commit with augmented .gitignore file from rails tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWebsite::Application.config.secret_token = '78ac0a2342d6807ca8060e86cab4ba4deccc88843fbad0633e104cee18da8e0623d54ce79e7f29cec15e03a9c3b0c131858374d17e53f9f442a9b9c248b84ac8'
