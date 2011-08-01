0fbf5bc5a5dab6b5e2dc117519d86af094164f68
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-08-01 20:54:02 +0100

    changed javascript so that submenu appears on hover over the arrow. added comments

--- public/javascripts/sexy_drop_down_menu.js
+++ public/javascripts/sexy_drop_down_menu.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
       
     $("ul.subnav").parent().append("<span></span>"); //Only shows drop down trigger when js is enabled (Adds empty span tag after ul.subnav*)  
      
-    $("ul.topnav li span").click(function() { //When trigger is clicked...  
+    $("ul.topnav li span").hover(function() { //When we hover over trigger...  
       
         //Following events are applied to the subnav itself (moving subnav up and down)  
         $(this).parent().find("ul.subnav").slideDown('fast').show(); //Drop down the subnav on click  
      
         $(this).parent().hover(function() {  
         }, function(){  
-            $(this).parent().find("ul.subnav").slideUp('slow'); //When the mouse hovers out of the subnav, move it back up  
+            $(this).parent().find("ul.subnav").slideUp('fast'); //When the mouse hovers out of the subnav, move it back up  
         });  
       
         //Following events are applied to the trigger (Hover events for the trigger)  
